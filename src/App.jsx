5df34e4117503b0cdf4001d69d2110534d90a62e
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-09-12 19:24:18 +0000

    lab37

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Categories from "./Components/Categories/Categories";
 import Products from "./Components/Products/Products";
+import SimpleCart from "./Components/SimpleCart/SimpleCart";
+// import { ChakraProvider } from '@chakra-ui/react'
 import "./App.css";
 export default (props) => {
-  return <>
-  <Header/>
-  <Categories/>
-  <Products/>
-  <Footer/>
-
-
- 
-  
-  </>;
+  return (
+    <>
+     {/* <ChakraProvider> */}
+      <Header />
+      <Categories />
+      <Products />
+      <SimpleCart />
+      <Footer />
+      {/* </ChakraProvider> */}
+    </>
+  );
 };
