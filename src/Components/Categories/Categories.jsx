9a44cbca2e3379003aa53919cfabab106105ba6e
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-09-14 16:19:43 +0000

    lab38

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { category } from "../../store/categories";
+import { category } from "../../store/actions";
 import "./style.scss";
 import Products from "../Products/Products";
 // import { Margin } from "@mui/icons-material";
@@ -11,14 +11,33 @@ function Categories(props) {
       <h4 style={{ marginTop: "-50px" }}>Browse our Categories</h4>
       <section>
         <span
-          style={{ marginRight: "6px",  }}
-          onClick={() => props.category("Games")}
+          style={{ marginRight: "6px" }}
+          onClick={() => props.category("GAMES")}
         >
           Games|{" "}
         </span>
-        <span onClick={() => props.category("Food") }>Food</span>
+        <span
+          onClick={() => props.category("FOOD")}
+          style={{ marginRight: "6px" }}
+        >
+          Food|{" "}
+        </span>
+        <span
+          onClick={() => props.category("WEAPONS")}
+          style={{ marginRight: "6px" }}
+        >
+          WEAPONS|{" "}
+        </span>
+        <span
+          onClick={() => props.category("ELECTRONICS")}
+          style={{ marginRight: "6px" }}
+        >
+          ELECTRONICS
+        </span>
 
-        <div className="category"><h3>{props.myCategory.activeCategory}</h3></div>
+        <div className="category">
+          <h3>{props.myCategory.activeCategory}</h3>
+        </div>
 
         {props.myCategory.categories.map((cate, idx) => {
           if (cate.normalizedName === props.myCategory.activeCategory) {
--- src/store/products.jsx
+++ src/store/products.jsx
@@ -1,40 +1,40 @@
 let initialState = {
   products: [
-    {
-      categoryAssociation: "Games",
-      name: "SMASH BROS",
-      description: "SMASH BROS Description.....",
-      price: "$1200",
-      inventoryCount: 500,
-    },
-    {
-      categoryAssociation: "Games",
-      name: "Monopoly",
-      description: "Monopoly Description......",
-      price: "$1000",
-      inventoryCount: 820,
-    },
-    {
-      categoryAssociation: "Food",
-      name: "Cookies",
-      description: "Cookies Description......",
-      price: "$2.38",
-      inventoryCount: 998,
-    },
-    {
-      categoryAssociation: "Food",
-      name: "Pizza",
-      description: "Pizza Description.......",
-      price: "$1.99",
-      inventoryCount: 703,
-    },
-    {
-      categoryAssociation: "Food",
-      name: "Banana",
-      description: "Banana Description...... ",
-      price: "$0.99",
-      inventoryCount: 534,
-    },
+    // {
+    //   categoryAssociation: "Games",
+    //   name: "SMASH BROS",
+    //   description: "SMASH BROS Description.....",
+    //   price: "$1200",
+    //   inventoryCount: 500,
+    // },
+    // {
+    //   categoryAssociation: "Games",
+    //   name: "Monopoly",
+    //   description: "Monopoly Description......",
+    //   price: "$1000",
+    //   inventoryCount: 820,
+    // },
+    // {
+    //   categoryAssociation: "Food",
+    //   name: "Cookies",
+    //   description: "Cookies Description......",
+    //   price: "$2.38",
+    //   inventoryCount: 998,
+    // },
+    // {
+    //   categoryAssociation: "Food",
+    //   name: "Pizza",
+    //   description: "Pizza Description.......",
+    //   price: "$1.99",
+    //   inventoryCount: 703,
+    // },
+    // {
+    //   categoryAssociation: "Food",
+    //   name: "Banana",
+    //   description: "Banana Description...... ",
+    //   price: "$0.99",
+    //   inventoryCount: 534,
+    // },
   ],
   // activeCategory: "",
 };
@@ -48,7 +48,7 @@ export default (state = initialState, action) => {
         if (product.name === payload.name) {
           return {
             ...product,
-            inventoryCount: product.inventoryCount - 1,
+            count: product.count - 1,
           };
         } else {
           return product;
@@ -62,7 +62,7 @@ export default (state = initialState, action) => {
         if (product.name === payload.name) {
           return {
             ...product,
-            inventoryCount: product.inventoryCount + 1,
+            count: product.count + 1,
           };
         } else {
           return product;
@@ -72,21 +72,26 @@ export default (state = initialState, action) => {
         products: newCount,
       };
     }
+    case "GET":
+      console.log(state.products)
+            return{
+              products:payload
+            }
     default:
       return state;
   }
 };
 
-export const product = (name) => {
-  return {
-    type: "FILTER",
-    payload: name,
-  };
-};
+// export const product = (name) => {
+//   return {
+//     type: "FILTER",
+//     payload: name,
+//   };
+// };
 
-export const addItem = (item) => {
-  return {
-    type: "ADD",
-    payload: item,
-  };
-};
+// export const addItem = (item) => {
+//   return {
+//     type: "ADD",
+//     payload: item,
+//   };
+// };
